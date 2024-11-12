5d4cd8be1e7a02c2fa5014e58e53e4fe9e34317e
Mads Hartwich <s235102@dtu.dk>
2024-11-12 10:39:21 +0100

    added functions to enviroment.java

--- Environment.java
+++ Environment.java
@@ -1,10 +1,13 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.List;
 import java.util.ArrayList;
 
 class Environment {
     // Map for the signals: each signal has a current value as a Boolean
+    private Map<String, Boolean> signalValues;
+    private Map<String, Def> functionDefinitions;
     private HashMap<String,Boolean> variableValues = new HashMap<String,Boolean>();
 
     // Map for defined functions like "xor": for a string like "xor",
@@ -13,7 +16,11 @@ class Environment {
 
     // Standard constructor that does not care about definitions
     // You cannot use getDef() below when using this constructor.
-    public Environment(){ this.defs=new HashMap<String,Def>();} 
+    public Environment(){
+        signalValues = new HashMap<>();
+        functionDefinitions = new HashMap<>();
+        this.defs = new HashMap<String,Def>();
+    }
 
     // Constructor that compute the map of function definitions, given
     // a the set of definitions as available in Circuit. You can then
@@ -27,7 +34,9 @@ class Environment {
     // This constructor can be used during eval to create a new
     // environment with the same definitions contained in an existing
     // one:
-    public Environment(Environment env) { this.defs=env.defs; }
+    public Environment(Environment env) {
+        this.defs=env.defs;
+    }
 
     // Lookup a definition, e.g., "xor"
     public Def getDef(String name){
@@ -63,5 +72,32 @@ class Environment {
 	}
 	return table;
     }
+
+    // Method to get the Boolean value of a signal by its name
+    public Boolean getSignalValue(String varname) {
+        return signalValues.get(varname);
+    }
+
+    // Method to set a Boolean value for a signal in the environment
+    public void setSignalValue(String varname, Boolean value) {
+        signalValues.put(varname, value);
+    }
+
+    // Method to add a function definition to the environment
+    public void addFunctionDefinition(String functionName, Def functionDef) {
+        functionDefinitions.put(functionName, functionDef);
+    }
+
+    // Method to get a function definition from the environment
+    public Def getFunctionDefinition(String functionName) {
+        return functionDefinitions.get(functionName);
+    }
+    public Environment createScope() {
+        Environment newEnv = new Environment();
+        newEnv.signalValues.putAll(this.signalValues);
+        newEnv.functionDefinitions.putAll(this.functionDefinitions);
+        return newEnv;
+    }
+
 }
 
